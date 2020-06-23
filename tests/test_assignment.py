e9d5992aa51ded366374954be0942aae89fee47f
jiangxiaofan-168 <58451904+jiangxiaofan-168@users.noreply.github.com>
2020-06-23 18:52:16 +0800

    在tests下完善相应的单元测试

--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -8,10 +8,41 @@ class AssignmentTest(unittest.TestCase):
     """
 
     def test_load_state(self):
-        CourseWareB.load_raw_state("")
+        #given
+        raw_state = {
+            "commonComponentState":{
+                "4cb5f12f9e164c6c545a55202bc818f2":{
+                    "answer":[1,3,0,3]}
+            }
+        }
 
+        # WHEN
+        result = CourseWareB().load_raw_state(json.dumps(raw_state))
+
+        # THEN
+        target_state = [1,3,0,3]
+        self.assertEqual(target_state, result)
+        
     def test_right_ans(self):
-        CourseWareB.is_user_right(())
+        # GIVEN
+        stream = [1,2,0,3]
+        # THEN
+        self.assertTrue(CourseWareB().is_user_right(stream))
+
+        stream = [2,3,1,4]
+        self.assertTrue(not CourseWareB().is_user_right(stream))
+
+        stream = [1,2,1,3]
+        self.assertTrue(not CourseWareB().is_user_right(stream))
+        
+    #其他测试   
+    def test_none_state(self):
+        
+        raw_state = {"commonComponentState":None}
+        result = CourseWareB().load_raw_state(json.dumps(raw_state))
+        
+        target_state = []
+        self.assertEqual(target_state, result)
 
 
 if __name__ == "__main__":
